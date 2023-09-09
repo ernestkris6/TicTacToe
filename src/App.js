6fd2f22012d48f3c53b584692e9fc1cf30523d61
ERNEST CHRIS <ernestchris6@gmail.com>
2023-09-09 21:42:58 +0100

    my new commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TicTacToe from './Components/Tic-Tac-Toe/TicTacToe';
+
+ const App =()=> {
+  return (
+    <div>
+
+    <TicTacToe />
+
+    </div>
+  )
+}
+
+
+export default App;
